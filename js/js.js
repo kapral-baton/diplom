baa1bdb01f6fec6b1f3d74496d1f73058dc6e3c7
Tima Ivanov <ivtima.bp@gmail.com>
2022-07-04 21:17:51 +0200

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,29 @@
+$('.slider').slick({
+  	infinite: true,
+  	slidesToShow: 3,
+	slidesToScroll: 3,
+  	responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+       
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 360,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ],
+});
